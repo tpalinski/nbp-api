828d79f637efb1dda2382100841fc3c5fb738c71
tpalinski <tpalinski@icloud.com>
2023-04-21 15:34:18 +0200

    Add basic express server

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,1 +1,12 @@
-console.log("Hello world")
+import express from 'express';
+
+
+const PORT = "8080";
+
+const app = express();
+
+
+
+app.listen(PORT, () => {
+	console.log(`Server running on port ${PORT}`)
+})
