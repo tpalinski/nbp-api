a1a97c27d4ab4be51db8b84b0e67ed51f739d765
tpalinski <tpalinski@icloud.com>
2023-04-21 15:44:42 +0200

    Add homepage

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,15 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import dotenv from 'dotenv';
 
 dotenv.config();
 const PORT =  process.env.PORT || "8080";
 const app = express();
+
+
+app.get("/", (req: Request, res: Response) => {
+	res.status(200);
+	res.send("Please refer to documentation at https://github.com/tpalinski/nbp-api for api-routes");
+})
 
 
 
