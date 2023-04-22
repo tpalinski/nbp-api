d23a34fcd2c302a8e2ba86a5633508f30ca4bdbf
tpalinski <tpalinski@icloud.com>
2023-04-22 14:41:41 +0200

    Add currency validation

--- server/src/routeParsers.ts
+++ server/src/routeParsers.ts
@@ -1,9 +1,16 @@
 import { NextFunction, Request, Response } from "express";
+import { isValidCode } from "./currencyRepository";
 
 export const currencyParser = async (req: Request, res: Response, next: NextFunction) => {
 	if(req.params.hasOwnProperty("currency")){
-		req.currency = req.params.currency;
-		next();
+		let currency = req.params.currency.toLowerCase()
+		if(isValidCode(currency)){
+			req.currency = req.params.currency;
+			next();
+		} else {
+			res.status(404);
+			res.send("Wrong currency code")
+		}
 	} else {
 		res.status(400);
 		res.send("No currency supplied")
